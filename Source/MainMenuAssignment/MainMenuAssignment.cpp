237354578df40994e8d1a1ed60d3714500931733
MintyMonster <camerongliddell@gmail.com>
2022-11-18 14:00:11 +0000

    First commit

---
+++ Source/MainMenuAssignment/MainMenuAssignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MainMenuAssignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MainMenuAssignment, "MainMenuAssignment" );
---
+++ Source/MainMenuAssignment/PlaybackDevices.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaybackDevices.h"
+
+PlaybackDevices::PlaybackDevices()
+{
+}
+
+PlaybackDevices::~PlaybackDevices()
+{
+}
